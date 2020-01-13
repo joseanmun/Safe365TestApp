ef4018b9b93fba22afc356b551c3bbbf51a9abd3
Anselmo <joseanmun@gmail.com>
2020-01-14 00:04:35 +0100

    Commit Test

--- app/src/main/java/com/example/mapapp/MapsActivity.kt
+++ app/src/main/java/com/example/mapapp/MapsActivity.kt
@@ -8,9 +8,8 @@
 
 package com.example.mapapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -25,6 +24,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
