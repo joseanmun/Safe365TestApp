30412da2e56099996a14c92e4a70ee93f34ac2ac
ANSELMO MUNOZ <anselmo.munozmedina@altran.com>
2020-01-13 23:58:28 +0100

    Add Copyright

--- data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
@@ -1,3 +1,11 @@
+/*
+ * Created by Anselmo Jose Munoz Medina on 1/13/20 11:57 PM
+ * Email: Joseanmun@gmail.com
+ * Copyright (c) 2020. All rights reserved.
+ * Last modified 1/13/20 11:50 PM
+ *
+ */
+
 package com.example.data
 
 import androidx.test.platform.app.InstrumentationRegistry
